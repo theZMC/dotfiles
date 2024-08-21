ebedc0ee50c53777ca5b880dfb3b18ca04b0be15
Zach Callahan <zach@zmc.dev>
2024-08-21 14:59:42 -0500

    nvim: make toggleterm float

--- .config/nvim/lua/plugins/astrocore.lua
+++ .config/nvim/lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -67,6 +65,12 @@ return {
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
       },
+    },
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    opts = {
+      direction = "float",
     },
   },
 }
