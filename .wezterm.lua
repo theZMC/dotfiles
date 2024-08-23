8db7d8ece7f49c7d88c39480c12500152c708b65
Zach Callahan <zach@zmc.dev>
2024-08-23 11:10:54 -0500

    wezterm: format config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,21 +1,21 @@
 local wezterm = require("wezterm")
 return {
-  font = wezterm.font({ family = "RecMonoCasual Nerd Font Mono", weight = 400}),
-  font_size = 16.0,
-  font_rules = {
-    {
-      italic = true,
-      font = wezterm.font_with_fallback({
-        { family = "Recursive", weight = 400, italic = true },
-        { family = "RecMonoCasual Nerd Font Mono", weight = 400, italic = true },
-      })
-    }
-  },
-  enable_tab_bar = false,
-  enable_wayland = true,
-  color_scheme = "Dark+",
-  window_background_opacity = 0.98,
-  macos_window_background_blur = 80,
-  win32_system_backdrop = "Acrylic",
-  front_end = "WebGpu",
+	font = wezterm.font({ family = "RecMonoCasual Nerd Font Mono", weight = 400 }),
+	font_size = 16.0,
+	font_rules = {
+		{
+			italic = true,
+			font = wezterm.font_with_fallback({
+				{ family = "Recursive",                    weight = 400, italic = true },
+				{ family = "RecMonoCasual Nerd Font Mono", weight = 400, italic = true },
+			}),
+		},
+	},
+	enable_tab_bar = false,
+	enable_wayland = true,
+	color_scheme = "Dark+",
+	window_background_opacity = 0.98,
+	macos_window_background_blur = 80,
+	win32_system_backdrop = "Acrylic",
+	front_end = "WebGpu",
 }
