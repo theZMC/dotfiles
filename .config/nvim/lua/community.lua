25c3254d422a2c513865163f29eaf1527bc5b5cd
Zach Callahan <zach@zmc.dev>
2024-10-05 13:51:11 -0500

    refactor(wezterm): move wezterm config to `.config` directory

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -5,7 +5,7 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
+  -- { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   { import = "astrocommunity.completion.copilot-cmp" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.refactoring-nvim" },
@@ -25,6 +25,7 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.proto" },
   { import = "astrocommunity.pack.ps1" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.svelte" },
@@ -33,6 +34,16 @@ return {
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.recipes.vscode" },
-  { import = "astrocommunity.test.neotest" },
+  {
+    import = "astrocommunity.test.neotest",
+    "nvim-neotest/neotest",
+    dependencies = {
+      "marilari88/neotest-vitest",
+    },
+    opts = function(_, opts)
+      if not opts.adapters then opts.adapters = {} end
+      table.insert(opts.adapters, require "neotest-vitest"(require("astrocore").plugin_opts "neotest-vitest"))
+    end,
+  },
   { import = "astrocommunity.test.nvim-coverage" },
 }
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,15 +6,15 @@ return {
 		{
 			italic = true,
 			font = wezterm.font_with_fallback({
-				{ family = "Recursive",                    weight = 400, italic = true },
+				{ family = "Recursive", weight = 400, italic = true },
 				{ family = "RecMonoCasual Nerd Font Mono", weight = 400, italic = true },
 			}),
 		},
 	},
 	enable_tab_bar = false,
 	enable_wayland = true,
-	color_scheme = "Dark+",
-	window_background_opacity = 0.98,
+	color_scheme = "Astrodark",
+	window_background_opacity = 0.85,
 	macos_window_background_blur = 80,
 	win32_system_backdrop = "Acrylic",
 	front_end = "WebGpu",
