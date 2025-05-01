58d7ce8e88fb68c2f193d469a83112dff573d402
Zach Callahan <zach@zmc.dev>
2025-05-01 15:01:47 -0500

    feat(nvim): configured formatters by filetype in conform

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -1,6 +1,10 @@
 ---@type LazySpec
 return {
-  { "Saghen/blink.cmp", version = "*", build = "cargo build --release" },
+  {
+    "Saghen/blink.cmp",
+    version = "*",
+    build = "cargo build --release",
+  },
   {
     "rachartier/tiny-inline-diagnostic.nvim",
     optional = true,
@@ -19,6 +23,36 @@ return {
       model = "claude-3.7-sonnet",
       window = {
         layout = "float",
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        markdown = { "deno_fmt" },
+        json = { "deno_fmt" },
+        javascript = { "deno_fmt" },
+        typescript = { "deno_fmt" },
+        jsx = { "deno_fmt" },
+        tsx = { "deno_fmt" },
+        css = { "deno_fmt" },
+        html = { "deno_fmt" },
+        scss = { "deno_fmt" },
+        sass = { "deno_fmt" },
+        less = { "deno_fmt" },
+        astro = { "deno_fmt" },
+        svelte = { "deno_fmt" },
+        vue = { "deno_fmt" },
+        sql = { "deno_fmt" },
+        yaml = { "yamlfmt" },
+        terraform = { "hclfmt" },
+      },
+      formatters = {
+        yamlfmt = {
+          prepend_args = { "-formatter", "indentless_arrays=true" },
+        },
       },
     },
   },
