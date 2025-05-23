10190fde985b649068a7a85604d97d4e4a09ce67
Zach Callahan <zach@zmc.dev>
2025-05-23 15:18:55 -0500

    refactor(wezterm): simplified fontconfig for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,16 +8,16 @@ local config = {}
 config.adjust_window_size_when_changing_font_size = false
 config.font = wezterm.font_with_fallback({
 	{ family = "RecMonoCasual Nerd Font Mono", weight = 400 },
-	{ family = "Symbols Nerd Font Mono", weight = 400 },
+	{ family = "Symbols Nerd Font Mono",       weight = 400 },
 })
 config.font_size = 16.0
 config.font_rules = {
 	{
 		italic = true,
 		font = wezterm.font_with_fallback({
-			{ family = "Recursive", weight = 400, italic = true },
+			{ family = "Recursive",                    weight = 400, italic = true },
 			{ family = "RecMonoCasual Nerd Font Mono", weight = 400, italic = true },
-			{ family = "Symbols Nerd Font Mono", weight = 400, italic = true },
+			{ family = "Symbols Nerd Font Mono",       weight = 400, italic = true },
 		}),
 	},
 }
