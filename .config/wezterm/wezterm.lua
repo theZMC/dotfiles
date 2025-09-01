b329da4ed2b3fb1891f54834886a4d0a67e95d6a
Zach Callahan <zach@zmc.dev>
2025-08-31 20:44:56 -0500

    refactor(wezterm): simplify font config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,28 +7,20 @@ local config = {}
 
 config.adjust_window_size_when_changing_font_size = false
 config.font = wezterm.font_with_fallback({
-	{ family = "RecMonoCasual Nerd Font Mono", weight = 400 },
-	{ family = "Symbols Nerd Font Mono",       weight = 400 },
+	{
+		family = "RecMonoCasual Nerd Font Mono",
+		weight = 400,
+		harfbuzz_features = {
+			"calt",
+		},
+	},
 })
 config.font_size = 16.0
-config.font_rules = {
-	{
-		italic = true,
-		font = wezterm.font_with_fallback({
-			{ family = "Recursive",                    weight = 400, italic = true },
-			{ family = "RecMonoCasual Nerd Font Mono", weight = 400, italic = true },
-			{ family = "Symbols Nerd Font Mono",       weight = 400, italic = true },
-		}),
-	},
-}
 config.enable_tab_bar = false
 if not is_linux then
 	config.color_scheme = "Astrodark"
 else
 	config.color_scheme = "Dark+"
-end
-if is_linux then
-	config.enable_wayland = true
 end
 if is_darwin then
 	config.macos_window_background_blur = 80
