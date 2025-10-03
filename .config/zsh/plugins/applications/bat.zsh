613a9a26104aab23c5fefb05aaa671777f06caac
Zach Callahan <zach@zmc.dev>
2025-10-02 19:51:37 -0500

    fix(zsh): update bat config to handle `batcat` as well as `bat`

--- .config/zsh/plugins/applications/bat.zsh
+++ .config/zsh/plugins/applications/bat.zsh
@@ -1,4 +1,7 @@
-if (( $+commands[bat] )); then
+if (( $+commands[bat] || $+commands[batcat] )); then
   export BAT_PAGER="less -RFX" 
+  if ! (( $+commands[bat] )); then
+    alias bat=batcat
+  fi
   alias cat=bat
 fi
