8e0631cf80624ed8706bd2f9c325ca46749d4e8d
Zach Callahan <zach@zmc.dev>
2025-10-12 20:16:02 -0500

    fix(zsh): correct k9s config dir variable expansion

--- .config/zsh/plugins/applications/k9s.zsh
+++ .config/zsh/plugins/applications/k9s.zsh
@@ -1,3 +1,3 @@
 if (( $+commands[k9s] )); then
-  export K9S_CONFIG_DIR="${K9S_CONFIG_DIR:-${XDG_CONFIG_DIR/k9s:-${HOME}/.config/k9s}}"
+  export K9S_CONFIG_DIR="${K9S_CONFIG_DIR:-${XDG_CONFIG_DIR:-${HOME}/.config}/k9s}"
 fi
