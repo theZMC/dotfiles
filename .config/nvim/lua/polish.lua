5a93628f145ab5ca6c6a6383cbfd77e2e17c40b4
Zach Callahan <zach@zmc.dev>
2026-01-08 16:53:24 -0600

    feat(nvim): add git diff copy keybinding

--- .config/nvim/lua/polish.lua
+++ .config/nvim/lua/polish.lua
@@ -1,5 +1,27 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+vim.keymap.set("n", "<leader>gy", function()
+  local file = vim.api.nvim_buf_get_name(0)
+  local diff = vim.fn.system("git diff -U5 HEAD -- " .. vim.fn.shellescape(file))
 
--- This will run last in the setup process.
--- This is just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
+  if vim.v.shell_error ~= 0 or diff == "" then
+    print "No changes found"
+    return
+  end
+
+  local lines = {}
+  for line in diff:gmatch "[^\n]+" do
+    if
+      not line:match "^diff "
+      and not line:match "^index "
+      and not line:match "^%-%-%-"
+      and not line:match "^%+%+%+"
+    then
+      table.insert(lines, line)
+    end
+  end
+
+  local clean_diff = table.concat(lines, "\n")
+  local markdown_diff = "```diff\n" .. clean_diff .. "\n```"
+
+  vim.fn.setreg("+", markdown_diff)
+  print "Git diff copied to clipboard!"
+end, { desc = "Copy git diff to clipboard (markdown)" })
