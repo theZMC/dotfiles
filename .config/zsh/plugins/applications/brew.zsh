7554c78b9784a7552df718cee62f7075168ac102
Zach Callahan <zach@zmc.dev>
2026-01-13 19:26:38 -0600

    fix(nvim): add brew gnubin path to PATH if brew available

--- .config/zsh/plugins/applications/brew.zsh
+++ .config/zsh/plugins/applications/brew.zsh
@@ -1,4 +1,7 @@
 if (( $+commands[brew] )); then
   source <(brew shellenv)
+  if [[ -d /opt/homebrew/opt/grep/libexec/gnubin ]]; then
+    PATH="/opt/homebrew/opt/grep/libexec/gnubin:$PATH"
+  fi
   export HOMEBREW_NO_ENV_HINTS=1
 fi
