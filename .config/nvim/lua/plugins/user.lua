69a7ec960a796daf5881cedba84b1adf9ad27f16
Zach Callahan <zach@zmc.dev>
2026-05-24 17:11:35 -0500

    refactor(nvim): disable source selector in neotree

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -94,4 +94,11 @@ return {
       }
     end,
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = function(_, opts)
+      opts.source_selector = { winbar = false, statusline = false }
+      return opts
+    end,
+  },
 }
