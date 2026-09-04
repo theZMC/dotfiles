bb2f847353bbc926742421e3c09e0b4995fef827
Zach Callahan <zach@zmc.dev>
2026-09-04 15:07:45 -0500

    refactor(nvim): remove copilot

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -9,7 +9,6 @@ return {
   { import = "astrocommunity.code-runner.molten-nvim" },
   { import = "astrocommunity.color.transparent-nvim" },
   { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
-  { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.docker.lazydocker" },
   { import = "astrocommunity.editing-support.conform-nvim" },
